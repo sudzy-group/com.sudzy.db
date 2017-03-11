b57f718bc1ef07fea1fe560d5ab032dcbbe7ad52
sudzy <rganor@remedypartners.com>
2017-03-12 01:44:50 +0200

    add auto generator

--- generator/generate-table.js
+++ generator/generate-table.js
@@ -10,7 +10,8 @@ _.forIn(entities, function(properties, name) {
         var comments = [];
         prop.validate && comments.push("Validated");
         prop.search_by && comments.push( "Searchable");
-        
+        prop.name == "id" && comments.push("Auto-generated")
+        prop.name == "created_at" && comments.push("Auto-generated")
         t.push([prop.name,'`' + prop.type + '`', prop.group, prop.description, comments.join(", ")]);
     })
     console.log(table(t));
