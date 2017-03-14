dfc2edfb86d4e4faebcc64162fa758897c52d68f
Hilary Barr <hilaryrosebarr@gmail.com>
2017-03-13 22:53:23 -0400

    Add delivery entity

--- src/entities/Delivery.ts
+++ src/entities/Delivery.ts
@@ -5,12 +5,56 @@ import { identity } from 'lodash';
  * Represent a Delivery entity
  */
 export class Delivery extends Entity {
+	
 	@EntityField({
         group: "default",
-        name: "test",
+        name: "customer_id",
         mandatory: true,
-        description: "test"
+        description: "Customer id"
     })
-    public test: string;
-	
+    public customer_id: string;
+
+    @EntityField({
+        group: "default",
+        name: "is_pickup",
+        mandatory: true,
+        description: "Whether delivery is pickup",
+        search_by: [ boolean ] 
+    })
+    public is_pickup: boolean;
+
+    @EntityField({
+        group: "time",
+        name: "delivery_time",
+        mandatory: true,
+        description: "Delivery time"
+    })
+    public delivery_time: string;
+
+    @EntityField({
+        group: "person",
+        name: "delivery_person",
+        description: "Delivery person"
+    })
+    public delivery_person: string;
+
+    @EntityField({
+        group: "status",
+        name: "is_confirmed",
+        description: "Whether store confirmed delivery"
+    })
+    public is_confirmed: boolean;
+
+    @EntityField({
+        group: "express",
+        name: "express_id",
+        description: "Express id"
+    })
+    public express_id: string;
+
+
+    protected boolean(is_pickup) {
+        //TODO boolean function
+        return true;
+    }
 }
