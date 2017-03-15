351442b82c1c5f04298386f74ac6c36998051697
Hilary Barr <hilaryrosebarr@gmail.com>
2017-03-15 13:32:49 -0400

    Customer delete tests

--- test/Customer-spec.ts
+++ test/Customer-spec.ts
@@ -26,7 +26,7 @@ class CustomerTest {
     const customers = new Customers(CustomerTest.db, Customer);
     expect(customers.getPrefix()).to.equal("customer");
   }
-
+//Insertion  
   @test("should insert customer with mobile")
   public testInsertMobile(done) {
     const customers = new Customers(CustomerTest.db, Customer);
@@ -66,7 +66,7 @@ class CustomerTest {
     }).catch(_.noop);
   }
 
-
+//Search
   @test("should be searchable by mobile")
   public testSearchMobile(done) {
     const customers = new Customers(CustomerTest.db, Customer);
@@ -113,8 +113,9 @@ class CustomerTest {
       expect(cs[0].name).to.equal("Roo Ganor");
       done();
     }).catch(_.noop);
-  }  
+  } 
 
+//Update
   @test("should update parameters")
   public testUpdate(done) {
     let customerObj = {
@@ -156,7 +157,90 @@ class CustomerTest {
       done();
     }).catch(_.noop);
   }
+
+  @test("should not update mobile")
+  public testNoMobileUpdate(done) {
+    let customerObj = {
+      mobile: "19292778392",
+      name: "Mud Park"
+    }
+    const customers = new Customers(CustomerTest.db, Customer);
+    customers.insert(customerObj).then((c) => {
+      expect(c.mobile).to.equal("19292778392");
+      let updatedCustomerObj = {
+        mobile: "19292778322"
+      } 
+      return customers.update(c, updatedCustomerObj);
+     }).then(_.noop)
+     .catch((c) => {
+      done();
+    });
+  }
+
   
+//Delete
+@test("should delete customer with just mobile")
+ public testDeleteCustomerMobile(done) {
+    let id = "";
+    let customerObj = {
+      mobile: "19292778387"
+    }
+    const customers = new Customers(CustomerTest.db, Customer);
+    customers.insert(customerObj).then((c) => {
+      id = c.id;
+      return customers.get(id);
+     }).then((cus) => {  
+      expect(cus.mobile).to.equal("19292778387");
+      return customers.remove(cus);
+     }).then((e) => {
+        return customers.get(id);
+     }).then(_.noop)
+     .catch((c) => {
+      done();
+    });
+  }
+
+
+ @test("should delete customer with many params")
+ public testDeleteCustomerAllParams(done) {
+    let id = "";
+    let customerObj = {
+      mobile: "19292778388",
+      name: "Joseph Shmoo",
+      email: "joesh@gmail.com",
+      autocomplete: "199 Orchard St, New York, NY 10002, USA",
+      street_num: "199",
+      street_route: "Orchard Street",
+      apartment: "2D",
+      city: "New York",
+      state: "NY",
+      zip: "10002",
+      lat: "40.72224",
+      lng: "-73.988152",
+      is_doorman: true,
+      delivery_notes: "Ring bell twice",
+      cleaning_notes: "Clean slowly",
+      payment_customer_id: "cus_9xJOnv9Enc98S",
+      payment_customer_token: "tok_19dOlrDMuhhpO1mOm4flWqa"
+    }
+    const customers = new Customers(CustomerTest.db, Customer);
+    customers.insert(customerObj).then((c) => {
+      id = c.id;
+      return customers.get(id);
+     }).then((cus) => {  
+      expect(cus.mobile).to.equal("19292778388");
+      return customers.remove(cus);
+     }).then((e) => {
+        return customers.get(id);
+     }).then(_.noop)
+     .catch((c) => {
+      done();
+    });
+  }
+
+  
+
+//Validators  
   @test("should allow valid email")
   public testValidEmail(done) {
     const customers = new Customers(CustomerTest.db, Customer);
