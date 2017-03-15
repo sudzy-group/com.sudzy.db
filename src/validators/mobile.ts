82969c453152e3f952bcc8d6f330b8a57f705bf9
Hilary Barr <hilaryrosebarr@gmail.com>
2017-03-15 18:28:01 -0400

    Phone regex

--- src/validators/mobile.ts
+++ src/validators/mobile.ts
@@ -1,7 +1,4 @@
 export function mobile(input) {
 	let phoneRegex =  /^[1-9][0-9]{9,14}$/;
-	console.log("regex for") 
-	console.log(input)
-	console.log(phoneRegex.test(input))
     return phoneRegex.test(input);
 }
