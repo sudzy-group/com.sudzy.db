2e61a85e61e2701dd2cf50b6647521e16dbf9d4d
Hilary Barr <hilaryrosebarr@gmail.com>
2017-03-24 14:47:32 -0400

    Add order_items table

--- etl/tables.sql
+++ etl/tables.sql
@@ -65,4 +65,67 @@ CREATE TABLE `etl_orders` (
 );
 
 
+CREATE TABLE `etl_order_items` (
+  `id` varchar(36) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `order_id` varchar(250) NOT NULL,
+  `item_id` varchar(250) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `total_price` DECIMAL(10,2)  DEFAULT NULL,
+  `quantity` int(5) DEFAULT NULL,
+  `notes` varchar(250) DEFAULT NULL,
+  `separate` tinyint(1) DEFAULT NULL,
+  `wash` tinyint(1) DEFAULT NULL,
+  `dry` tinyint(1) DEFAULT NULL,
+  `detergent` varchar(100) DEFAULT NULL,
+  `color` varchar(100) DEFAULT NULL,
+  `pattern` varchar(100) DEFAULT NULL,
+  `brand` varchar(100) DEFAULT NULL,
+  `fabric` varchar(100) DEFAULT NULL,
+  `alteration_type` varchar(100) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `id_UNIQUE` (`id`)
+);
 
+
+
+
+
+
+
+
+
+-- # OrderTag
+-- | Field Name | Field Type | Group   | Description              | Comments       |
+-- | ---------- | ---------- | ------- | ------------------------ | -------------- |
+-- | id         | `string`   | default | Entity id                | Auto-generated |
+-- | created_at | `number`   | default | Creation datetime (unix) | Auto-generated |
+-- | order_id   | `string`   | default | Order id                 | Searchable     |
+-- | tag_number | `number`   | default | Tag number               |                |
+
+-- # OrderCharge
+-- | Field Name      | Field Type | Group   | Description                                    | Comments       |
+-- | --------------- | ---------- | ------- | ---------------------------------------------- | -------------- |
+-- | id              | `string`   | default | Entity id                                      | Auto-generated |
+-- | created_at      | `number`   | default | Creation datetime (unix)                       | Auto-generated |
+-- | order_id        | `string`   | default | Order id                                       | Searchable     |
+-- | amount          | `number`   | default | Amount charged                                 |                |
+-- | charge_type     | `string`   | default | Charge type of cash, credit, other             |                |
+-- | charge_id       | `string`   | card    | Charge id                                      | Validated      |
+-- | card_id         | `string`   | card    | Card id                                        | Validated      |
+-- | date_cash       | `string`   | cash    | Get all cash deposited today in drawer if cash |                |
+-- | refund_id       | `string`   | refund  | Refund id                                      |                |
+-- | amount_refunded | `number`   | refund  | Amount refunded                                |                |
+
+-- # Delivery
+-- | Field Name      | Field Type | Group   | Description                      | Comments       |
+-- | --------------- | ---------- | ------- | -------------------------------- | -------------- |
+-- | id              | `string`   | default | Entity id                        | Auto-generated |
+-- | created_at      | `number`   | default | Creation datetime (unix)         | Auto-generated |
+-- | customer_id     | `string`   | default | Customer id                      |                |
+-- | is_pickup       | `boolean`  | default | Whether delivery is pickup       |                |
+-- | delivery_time   | `string`   | default | Delivery time                    | Searchable     |
+-- | delivery_person | `string`   | person  | Delivery person                  | Validated      |
+-- | is_confirmed    | `boolean`  | status  | Whether store confirmed delivery |                |
+-- | is_canceled     | `boolean`  | status  | Whether delivery is canceled     |                |
+-- | express_id      | `string`   | express | Express id                       | Validated      |
