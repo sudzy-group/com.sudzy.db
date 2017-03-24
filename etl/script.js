9169f978c6db83e3348d68de3a359bb4053a2603
Hilary Barr <hilaryrosebarr@gmail.com>
2017-03-24 16:13:52 -0400

    Only insert if exists

--- etl/script.js
+++ etl/script.js
@@ -80,166 +80,187 @@ function copyPouchToSQL() {
         ts_promise_1["default"].all(ps).then(function () {
             return customers.find("name", "", { startsWith: true });
         }).then(function (cs) {
-            //1. Copy customers from pouch to sql    	
-            _.each(cs, function (customer) {
-                var cus = {
-                    id: customer.id,
-                    mobile: customer.mobile,
-                    name: customer.name,
-                    email: customer.email,
-                    autocomplete: customer.autocomplete,
-                    street_num: customer.street_num,
-                    street_route: customer.street_route,
-                    apartment: customer.apartment,
-                    city: customer.city,
-                    state: customer.state,
-                    zip: customer.zip,
-                    lat: customer.lat,
-                    lng: customer.lng,
-                    delivery_notes: customer.delivery_notes,
-                    cleaning_notes: customer.cleaning_notes,
-                    payment_customer_token: customer.payment_customer_token,
-                    payment_customer_id: customer.payment_customer_id,
-                    is_doorman: customer.is_doorman ? 1 : 0
-                };
-                var query = SQLconnection.query('INSERT INTO etl_customers SET ?', cus, function (error, results, fields) {
-                    if (error)
-                        throw error;
+            //1. Copy customers from pouch to sql
+            if (cs.length > 0) {
+                _.each(cs, function (customer) {
+                    var cus = {
+                        id: customer.id,
+                        mobile: customer.mobile,
+                        name: customer.name,
+                        email: customer.email,
+                        autocomplete: customer.autocomplete,
+                        street_num: customer.street_num,
+                        street_route: customer.street_route,
+                        apartment: customer.apartment,
+                        city: customer.city,
+                        state: customer.state,
+                        zip: customer.zip,
+                        lat: customer.lat,
+                        lng: customer.lng,
+                        delivery_notes: customer.delivery_notes,
+                        cleaning_notes: customer.cleaning_notes,
+                        payment_customer_token: customer.payment_customer_token,
+                        payment_customer_id: customer.payment_customer_id,
+                        is_doorman: customer.is_doorman ? 1 : 0
+                    };
+                    var query = SQLconnection.query('INSERT INTO etl_customers SET ?', cus, function (error, results, fields) {
+                        if (error)
+                            throw error;
+                    });
                 });
-            });
+            }
             return customer_cards.find("customer_id", "", { startsWith: true });
         }).then(function (crds) {
-            //2. Copy customer cards from pouch to sql  
-            _.each(crds, function (card) {
-                var crd = {
-                    id: card.id,
-                    customer_id: card.customer_id,
-                    card_id: card.card_id,
-                    brand: card.brand,
-                    last4: card.last4,
-                    is_default: card.is_default ? 1 : 0
-                };
-                var query = SQLconnection.query('INSERT INTO etl_customer_cards SET ?', crd, function (error, results, fields) {
-                    if (error)
-                        throw error;
+            //2. Copy customer cards from pouch to sql 
+            if (crds.length > 0) {
+                _.each(crds, function (card) {
+                    var crd = {
+                        id: card.id,
+                        customer_id: card.customer_id,
+                        card_id: card.card_id,
+                        brand: card.brand,
+                        last4: card.last4,
+                        is_default: card.is_default ? 1 : 0
+                    };
+                    var query = SQLconnection.query('INSERT INTO etl_customer_cards SET ?', crd, function (error, results, fields) {
+                        if (error)
+                            throw error;
+                    });
                 });
-            });
+            }
             return orders.find("customer_id", "", { startsWith: true });
         }).then(function (ords) {
-            //3. Copy orders from pouch to sql	     
-            _.each(ords, function (order) {
-                var ord = {
-                    id: order.id,
-                    customer_id: order.customer_id,
-                    readable_id: order.readable_id,
-                    due_datetime: order.due_datetime ? new Date(order.due_datetime) : null,
-                    rack: order.rack,
-                    notes: order.notes,
-                    tax: order.tax,
-                    tip: order.tip,
-                    discount_percent: order.discount_percent,
-                    discount_fixed: order.discount_fixed,
-                    balance: order.balance,
-                    all_ready: order.all_ready ? 1 : 0,
-                    all_pickedup: order.all_pickedup ? 1 : 0,
-                    delivery_pickup_id: order.delivery_pickup_id,
-                    delivery_dropoff_id: order.delivery_dropoff_id
-                };
-                var query = SQLconnection.query('INSERT INTO etl_orders SET ?', ord, function (error, results, fields) {
-                    if (error)
-                        throw error;
+            //3. Copy orders from pouch to sql	   
+            if (ords.length > 0) {
+                _.each(ords, function (order) {
+                    var ord = {
+                        id: order.id,
+                        customer_id: order.customer_id,
+                        readable_id: order.readable_id,
+                        due_datetime: order.due_datetime ? new Date(order.due_datetime) : null,
+                        rack: order.rack,
+                        notes: order.notes,
+                        tax: order.tax,
+                        tip: order.tip,
+                        discount_percent: order.discount_percent,
+                        discount_fixed: order.discount_fixed,
+                        balance: order.balance,
+                        all_ready: order.all_ready ? 1 : 0,
+                        all_pickedup: order.all_pickedup ? 1 : 0,
+                        delivery_pickup_id: order.delivery_pickup_id,
+                        delivery_dropoff_id: order.delivery_dropoff_id
+                    };
+                    var query = SQLconnection.query('INSERT INTO etl_orders SET ?', ord, function (error, results, fields) {
+                        if (error)
+                            throw error;
+                    });
                 });
-            });
+            }
             return order_items.find("order_id", "", { startsWith: true });
         }).then(function (ord_items) {
             //4. Copy order items from pouch to sql	
-            _.each(ord_items, function (order_item) {
-                var ord_item = {
-                    id: order_item.id,
-                    order_id: order_item.order_id,
-                    item_id: order_item.item_id,
-                    name: order_item.name,
-                    total_price: order_item.total_price,
-                    quantity: order_item.quantity,
-                    notes: order_item.notes,
-                    separate: order_item.separate ? 1 : 0,
-                    wash: order_item.wash ? 1 : 0,
-                    dry: order_item.dry ? 1 : 0,
-                    detergent: order_item.detergent,
-                    color: order_item.color,
-                    pattern: order_item.pattern,
-                    brand: order_item.brand,
-                    fabric: order_item.fabric,
-                    alteration_type: order_item.alteration_type
-                };
-                var query = SQLconnection.query('INSERT INTO etl_order_items SET ?', ord_item, function (error, results, fields) {
-                    if (error)
-                        throw error;
+            if (ord_items.length > 0) {
+                _.each(ord_items, function (order_item) {
+                    var ord_item = {
+                        id: order_item.id,
+                        order_id: order_item.order_id,
+                        item_id: order_item.item_id,
+                        name: order_item.name,
+                        total_price: order_item.total_price,
+                        quantity: order_item.quantity,
+                        notes: order_item.notes,
+                        separate: order_item.separate ? 1 : 0,
+                        wash: order_item.wash ? 1 : 0,
+                        dry: order_item.dry ? 1 : 0,
+                        detergent: order_item.detergent,
+                        color: order_item.color,
+                        pattern: order_item.pattern,
+                        brand: order_item.brand,
+                        fabric: order_item.fabric,
+                        alteration_type: order_item.alteration_type
+                    };
+                    var query = SQLconnection.query('INSERT INTO etl_order_items SET ?', ord_item, function (error, results, fields) {
+                        if (error)
+                            throw error;
+                    });
                 });
-            });
+            }
             return order_tags.find("order_id", "", { startsWith: true });
         }).then(function (ord_tags) {
             //5. Copy order tagss from pouch to sql	
-            _.each(ord_tags, function (order_tag) {
-                var ord_tag = {
-                    id: order_tag.id,
-                    order_id: order_tag.order_id,
-                    tag_number: order_tag.tag_number
-                };
-                var query = SQLconnection.query('INSERT INTO etl_order_tags SET ?', ord_tag, function (error, results, fields) {
-                    if (error)
-                        throw error;
+            if (ord_tags.length > 0) {
+                _.each(ord_tags, function (order_tag) {
+                    var ord_tag = {
+                        id: order_tag.id,
+                        order_id: order_tag.order_id,
+                        tag_number: order_tag.tag_number
+                    };
+                    var query = SQLconnection.query('INSERT INTO etl_order_tags SET ?', ord_tag, function (error, results, fields) {
+                        if (error)
+                            throw error;
+                    });
                 });
-            });
+            }
             return order_charges.find("order_id", "", { startsWith: true });
         }).then(function (ord_charges) {
             //6. Copy order charges from pouch to sql	
-            _.each(ord_charges, function (order_charge) {
-                var ord_charge = {
-                    id: order_charge.id,
-                    order_id: order_charge.order_id,
-                    amount: order_charge.amount,
-                    charge_type: order_charge.charge_type,
-                    charge_id: order_charge.charge_id,
-                    card_id: order_charge.card_id,
-                    date_cash: order_charge.date_cash ? new Date(order_charge.date_cash) : null,
-                    refund_id: order_charge.refund_id,
-                    amount_refunded: order_charge.amount_refunded
-                };
-                var query = SQLconnection.query('INSERT INTO etl_order_charges SET ?', ord_charge, function (error, results, fields) {
-                    if (error)
-                        throw error;
+            if (ord_charges.length > 0) {
+                _.each(ord_charges, function (order_charge) {
+                    var ord_charge = {
+                        id: order_charge.id,
+                        order_id: order_charge.order_id,
+                        amount: order_charge.amount,
+                        charge_type: order_charge.charge_type,
+                        charge_id: order_charge.charge_id,
+                        card_id: order_charge.card_id,
+                        date_cash: order_charge.date_cash ? new Date(order_charge.date_cash) : null,
+                        refund_id: order_charge.refund_id,
+                        amount_refunded: order_charge.amount_refunded
+                    };
+                    var query = SQLconnection.query('INSERT INTO etl_order_charges SET ?', ord_charge, function (error, results, fields) {
+                        if (error)
+                            throw error;
+                    });
                 });
-            });
+            }
             return deliveries.find("delivery_time", "", { startsWith: true });
         }).then(function (delivs) {
-            //7. Copy deliveriesfrom pouch to sql	
-            var amount = delivs.length;
-            var i = 0;
-            _.each(delivs, function (delivery) {
-                var deliv = {
-                    id: delivery.id,
-                    customer_id: delivery.customer_id,
-                    is_pickup: delivery.is_pickup ? 1 : 0,
-                    delivery_time: new Date(delivery.delivery_time),
-                    delivery_person: delivery.delivery_person,
-                    is_confirmed: delivery.is_confirmed ? 1 : 0,
-                    is_canceled: delivery.is_canceled ? 1 : 0,
-                    express_id: delivery.express_id
-                };
-                var query = SQLconnection.query('INSERT INTO etl_deliveries SET ?', deliv, function (error, results, fields) {
-                    if (error)
-                        throw error;
-                    i++;
-                    if (i == amount) {
-                        console.log("About to disconnect");
-                        disconnectSQL();
-                        if (config.mocks) {
-                            destroyPouch();
+            //7. Copy deliveries from pouch to sql	
+            if (delivs.length > 0) {
+                var amount_1 = delivs.length;
+                var i_1 = 0;
+                _.each(delivs, function (delivery) {
+                    var deliv = {
+                        id: delivery.id,
+                        customer_id: delivery.customer_id,
+                        is_pickup: delivery.is_pickup ? 1 : 0,
+                        delivery_time: new Date(delivery.delivery_time),
+                        delivery_person: delivery.delivery_person,
+                        is_confirmed: delivery.is_confirmed ? 1 : 0,
+                        is_canceled: delivery.is_canceled ? 1 : 0,
+                        express_id: delivery.express_id
+                    };
+                    var query = SQLconnection.query('INSERT INTO etl_deliveries SET ?', deliv, function (error, results, fields) {
+                        if (error)
+                            throw error;
+                        i_1++;
+                        if (i_1 == amount_1) {
+                            console.log("About to disconnect");
+                            disconnectSQL();
+                            if (config.mocks) {
+                                destroyPouch();
+                            }
                         }
-                    }
+                    });
                 });
-            });
+            }
+            else {
+                console.log("About to disconnect");
+                disconnectSQL();
+                if (config.mocks) {
+                    destroyPouch();
+                }
+            }
             done();
         })["catch"](_.noop);
     });
