81174fdf1dfd1c997745661778e272584bbb8c6b
Roy Ganor <ganoro@gmail.com>
2017-03-29 16:20:19 +0300

    export statements

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,22 @@
  * Sudzy Database Exports
  */
 export { Customers } from "./collections/Customers";
-export {CustomerCards} from "./collections/CustomerCards";
-export {Orders} from "./collections/Orders";
-export {OrderItems} from "./collections/OrderItems";
-export {OrderTags} from "./collections/OrderTags";
-export {OrderCharges} from "./collections/OrderCharges";
-export {Deliveries} from "./collections/Deliveries";
+export { CustomerCards } from "./collections/CustomerCards";
+export { Orders } from "./collections/Orders";
+export { OrderItems } from "./collections/OrderItems";
+export { OrderTags } from "./collections/OrderTags";
+export { OrderCharges } from "./collections/OrderCharges";
+export { Deliveries } from "./collections/Deliveries";
 
-export {Customer} from "./entities/Customer";
-export {CustomerCard} from "./entities/CustomerCard";
-export {Order} from "./entities/Order";
-export {OrderItem} from "./entities/OrderItem";
-export {OrderTag} from "./entities/OrderTag";
-export {OrderCharge} from "./entities/OrderCharge";
-export {Delivery} from "./entities/Delivery";
+export { Customer } from "./entities/Customer";
+export { CustomerCard } from "./entities/CustomerCard";
+export { Order } from "./entities/Order";
+export { OrderItem } from "./entities/OrderItem";
+export { OrderTag } from "./entities/OrderTag";
+export { OrderCharge } from "./entities/OrderCharge";
+export { Delivery } from "./entities/Delivery";
+
+export { Store } from './config/Store';
+export { Item, Items } from './config/Items';
+
+export { Database } from './access/Database';
