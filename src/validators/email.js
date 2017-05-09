4fd5acba9a7d2fd039752a4f6a51f75595acf539
Hilary Barr <hilaryrosebarr@gmail.com>
2017-05-09 17:26:55 -0400

    Revert "Add in_stripe to card"
    This reverts commit ea8936eaae2925d5da8604a6dae647d2f20308a5.

--- src/validators/email.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function email(input) {
-    var emailRegex = /^[A-Za-z0-9._]*\@[A-Za-z]*\.[A-Za-z]{2,5}$/;
-    return emailRegex.test(input);
-}
-exports.email = email;
--- src/validators/noWhitespace.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function noWhitespace(input) {
-    return !/\s/g.test(input);
-}
-exports.noWhitespace = noWhitespace;
