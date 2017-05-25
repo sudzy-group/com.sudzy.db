0b25cf2d58592679bb4fc1eb7fd3f31e602dc0ba
Roy Ganor <ganoro@gmail.com>
2017-05-25 20:20:59 +0300

    add summary table

---
+++ etl/summary_tables.sql
@@ -0,0 +1,32 @@
+########################
+# Orders count
+########################
+DROP TABLE IF EXISTS `{{store_id}}_orders_count`;
+
+CREATE TABLE `{{store_id}}_orders_count` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day` date NOT NULL,
+  `orders_count` INT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `{{store_id}}_orders_count` (day, orders_count) 
+SELECT date(created_at) as day, count(date(created_at)) as orders_count FROM `{{store_id}}_orders` group by date(created_at);
+
+########################
+# Payments count
+########################
+
+DROP TABLE IF EXISTS `{{store_id}}_payments`;
+
+CREATE TABLE `{{store_id}}_payments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day` date NOT NULL,
+  `counts` INT NULL,
+  `sums` DOUBLE NULL,
+  `avgs` DOUBLE NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `{{store_id}}_payments` (day, counts, sums, avgs) 
+SELECT date(created_at) as day, count(date(created_at)) as counts, sum(amount) as sums, avg(amount) as avgs FROM `{{store_id}}_order_charges` group by date(created_at);
