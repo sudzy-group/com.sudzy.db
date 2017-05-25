d71f7fc678dc1b1d84c2addd93a9398d35ddbe9d
Roy Ganor <ganoro@gmail.com>
2017-05-25 22:56:08 +0300

    etc update

--- etl/summary_tables.sql
+++ etl/summary_tables.sql
@@ -14,6 +14,21 @@ INSERT INTO `{{store_id}}_orders_count` (day, orders_count)
 SELECT date(created_at) as day, count(date(created_at)) as orders_count FROM `{{store_id}}_orders` group by date(created_at);
 
 ########################
+# Orders pricing
+########################
+DROP TABLE IF EXISTS `{{store_id}}_orders_pricing`;
+
+CREATE TABLE `{{store_id}}_orders_pricing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day` date NOT NULL,
+  `orders_count` INT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `{{store_id}}_orders_pricing` (original_id, day, items_count, total_order_price) 
+SELECT original_id, date(created_at) as day, count(id) as items_count, sum(price) as total_order_price FROM `{{store_id}}_order_items` group by order_id;
+
+########################
 # Payments count
 ########################
 
