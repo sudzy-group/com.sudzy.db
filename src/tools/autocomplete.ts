f07186f4b7732db2d1a1c4b644fa522831357d53
Roy Ganor <ganoro@gmail.com>
2017-06-18 00:20:43 +0300

    add imports

---
+++ src/tools/autocomplete.ts
@@ -0,0 +1,161 @@
+import { Promise } from 'ts-promise';
+import { map, forIn } from 'lodash';
+import * as https from 'https';
+import * as commander from 'commander';
+import * as csv from 'csvtojson';
+import * as util from 'util';
+import * as querystring from 'querystring';
+import * as async from 'async';
+
+let p = commander
+  .version('0.0.1')
+  .usage('[options]')
+	.option('-f, --autocompleteFile [value]', 'The addresses csv file')
+	.parse(process.argv);
+
+if (!p.autocompleteFile) {
+   console.error('no addresses arguments given.');
+   process.exit(1);
+}
+
+/**
+ * Google Autocomplete Service to help with typeahead of addresses
+ * 
+ * @export
+ * @class GoogleAutocompleteService
+ */
+export class GoogleAutocompleteService {
+
+	/**
+	 * Get google autocomplete suggestions
+	 * @param {string} input 
+	 * @param {any} center 
+	 * @returns {Promise<Place[]>} 
+	 */
+	public autocomplete(input: string, center): Promise<Place[]> {
+
+		return new Promise<Place[]>((resolve, reject) => {
+			var queryParams = querystring.stringify({input: input, key: 'AIzaSyBnVZV3mK99RV8RjOZEc32mMjYIkbcYC7w', location: center.latitude + ',' + center.longitude, radius : '3000'});
+			let url = this._getHost() + '/maps/api/place/autocomplete/json?';
+			this.get(url +  queryParams,
+				body => {
+					let result = JSON.parse(body);
+					return result.predictions ? resolve(this.toPlaces(result.predictions)) : reject(null);
+				}
+			);
+		});
+	}
+
+	/**
+	 * Get details for a given place
+	 * @param {Place} place 
+	 * @param {any} center 
+	 * @returns {Promise<any>} 
+	 */
+	public details(place: Place): Promise<Place> {
+		return new Promise<Place>((resolve, reject) => {
+			if (!place) {
+				resolve(null);
+			}
+			var queryParams = querystring.stringify({key: 'AIzaSyBnVZV3mK99RV8RjOZEc32mMjYIkbcYC7w', placeid: place.placeid});
+			let url = this._getHost() + '/maps/api/place/details/json?';
+			this.get(url + queryParams, 
+				body => {
+					let result = JSON.parse(body);
+					return result.result ? resolve(result.result) : reject(null);
+				}
+			);
+		});
+	}
+
+	private _getHost() {
+		return 'https://maps.googleapis.com';
+	}
+
+	private toPlaces(predictions: any[]): Place[] {
+		return map(predictions, (p) => {
+			return {
+				placeid: p.place_id,
+				full: p.description,
+			};
+		})
+	}
+
+  private get(url, callback) {
+    return https.get(url, function(response) {
+        var body = '';
+        response.on('data', function(d) {
+          body += d;
+        });
+        response.on('end', function() {
+          callback(body);
+        });
+    });
+
+  }
+}
+
+export interface Place {
+	placeid: string;
+	full: string;
+	street?: string;
+	city?: string;
+	state?: string;
+	country?: string;
+	zipcode?: string;
+}
+
+function loadAddresses(callback) {
+	let customer = [];
+	csv()
+	.fromFile(p.autocompleteFile)
+	.on('json',(jsonObj)=>{
+		customer.push(jsonObj);
+	})
+	.on('done',(error)=>{
+		callback(customer);
+	})
+}
+
+function toPlace(result) {
+	let output = {};
+	let cs =  result.address_components;
+	cs.forEach(c => {
+		output[c.types[0]] = c.short_name;
+	})
+	output['autocomplete'] = result.formatted_address;
+	if (result.geometry && result.geometry.location) {
+		output['lat'] = result.geometry.location.lat;
+		output['lng'] = result.geometry.location.lng;
+	}
+	return output;
+}
+
+function resolveAddress(address) {
+	return (callback) => {
+		acs.autocomplete(address, {"longitude": -73.977574, "latitude": 40.726235}).then(ps => {
+			if (ps && ps.length > 0) {
+				return acs.details(ps[0]).then(d => {
+					let p: any = toPlace(d);
+					console.log('"' + p.autocomplete + '","' + p.street_number + '","' + p.route + '","' + p.locality + '","' + p.administrative_area_level_1 + '","' + p.country + '","' + p.postal_code + '",' +p.lat + ',' + p.lng);
+					return callback(null, '"' + p.autocomplete + '","' + p.street_number + '","' + p.route + '","' + p.locality + '","' + p.administrative_area_level_1 + '","' + p.country + '","' + p.postal_code + '"');
+				})
+			};
+			console.log("")
+			return callback(null, "");
+		})	
+	}
+}
+
+let acs = new GoogleAutocompleteService();
+loadAddresses(ads => {
+	let ps = [];
+	ads.forEach(a => {
+		ps.push(resolveAddress(a.address));
+	})
+	async.series(ps, // optional callback
+		function(err, results) {
+				console.log(err, results)
+		}
+	);
+})
---
+++ src/tools/importCustomers.ts
@@ -0,0 +1,139 @@
+import * as PouchDB from "pouchdb";
+import * as PouchableAuthentication from 'pouchdb-authentication';
+PouchDB.plugin(PouchableAuthentication);
+
+import * as csv from 'csvtojson';
+import { PhoneNumberUtil } from 'google-libphonenumber';
+
+import * as _ from 'lodash';
+import Promise from "ts-promise";
+import * as mysql from "mysql";
+
+import { Customers } from "../collections/Customers";
+import { CustomerCards } from "../collections/CustomerCards";
+import { Orders } from "../collections/Orders";
+import { OrderItems } from "../collections/OrderItems";
+import { OrderTags } from "../collections/OrderTags";
+import { OrderCharges } from "../collections/OrderCharges";
+import { Deliveries } from "../collections/Deliveries";
+
+import { Customer } from "../entities/Customer";
+import { CustomerCard } from "../entities/CustomerCard";
+import { Order } from "../entities/Order";
+import { OrderItem } from "../entities/OrderItem";
+import { OrderTag } from "../entities/OrderTag";
+import { OrderCharge } from "../entities/OrderCharge";
+import { Delivery } from "../entities/Delivery";
+import { Database } from '../access/Database';
+import * as commander from 'commander';
+
+let p = commander
+  .version('0.0.1')
+  .usage('[options]')
+  .option('-p, --remotePouchDB [value]', 'The remote PouchDB url')
+  .option('-U, --remotePouchDBUser [value]', 'The remote PouchDB user')
+  .option('-P, --remotePouchDBPassword [value]', 'The remote PouchDB password')
+	.option('-s, --storeId [value]', 'The store user')
+	.option('-f, --customersFile [value]', 'The customers csv file')
+	.parse(process.argv);
+
+if (!p.remotePouchDB || !p.storeId) {
+   console.error('no databases arguments given.');
+   process.exit(1);
+}
+
+let util = PhoneNumberUtil.getInstance();
+let countryCode = "+1";
+let region = "US";
+
+var pouch, customers: Customers;
+let database = new Database('default');
+
+connectPouch( () => {
+	loadCustomers(cs => {
+		
+		let ps = [];
+		cs.forEach(c => {
+			c.created_at = new Date(c.created_at).getTime();
+			let customer = {
+				name: c.name,
+				mobile: c.mobile,
+				formatted_mobile: format(c.mobile),
+			}
+			if (c.email) {
+				customer['email'] = c.email;
+			}
+			if (c.autocomplete) {
+				customer['autocomplete'] = c.autocomplete;
+				customer['street_num'] = c.street_num;
+				customer['street_route'] = c.street_route;
+				customer['city'] = c.city;
+				customer['state'] = c.state;
+				customer['zip'] = c.zip;
+				customer['lat'] = c.lat;
+				customer['lng'] = c.lng;
+				customer['apartment'] = c.apartment;
+			}
+			if (c.delivery_notes) {
+				customer['delivery_notes'] = c.delivery_notes;
+			}
+			if (c.cleaning_notes) {
+				customer['cleaning_notes'] = c.cleaning_notes;
+			}
+			ps.push(customers.insert(customer, c.created_at)) ;
+		});
+		Promise.all(ps).then(
+			css => {
+				database.sync().on('complete', () => {
+					console.log('sync done');
+				}).on('error', m => console.log(m));
+			}
+		);
+	});
+});
+
+function connectPouch(callback) {
+	console.log("remote pouch db", p.remotePouchDB);
+	database.connect(p.remotePouchDB, p.storeId, p.remotePouchDBUser, p.remotePouchDBPassword).then(() => {
+		database.sync().on('complete', () => {
+			customers = new Customers(database.db, Customer);
+			callback();
+		}).on('error', m => console.log(m));
+	}).catch(m => console.log(m));
+}
+
+function loadCustomers(callback) {
+	let customer = [];
+	csv()
+	.fromFile(p.customersFile)
+	.on('json',(jsonObj)=>{
+		customer.push(jsonObj);
+	})
+	.on('done',(error)=>{
+		callback(customer);
+	})
+}
+
+function toString(val) {
+	if (!val) {
+		return null;
+	}
+	if (_.isString(val)) {
+		return val;
+	}
+	if (_.isArray(val)) {
+		return val.join(', ');
+	}
+}
+
+
+function format(value) {
+	if (!value) { return '' };
+	if (value[0] != '+') {
+      value = countryCode + value;
+	}
+
+	let parsed = util.parse(value, region);
+	let formatted = util.format(parsed, 2);
+	return formatted;
+}
