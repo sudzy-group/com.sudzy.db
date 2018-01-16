4fadb06e5ddc814d5719bd54920ef19053c45ee8
Roy Ganor <ganoro@gmail.com>
2018-01-16 15:31:08 +0200

    fix factory script

--- etl/factory_etl.sh
+++ etl/factory_etl.sh
@@ -10,7 +10,7 @@ do_etl()
   sed s/\{\{store_id\}\}/$store/g etl/factory_tables.sql > etl/factory_tables_$store.sql
   sed s/\{\{store_id\}\}/$store/g etl/factory_summary_tables.sql > etl/factory_summary_tables_$store.sql
   mysql -h $mysql -D etl -u $user -p$pass < etl/factory_tables_$store.sql
-  lib/tools/factory_etl.js -p "$couchdb/$store\_factory" -P $store -U $store -h "$mysql" -u $user -w $pass -d etl -s $store
+  lib/tools/factory_etl.js -p "$couchdb/${store}_factory" -P $store -U $store -h "$mysql" -u $user -w $pass -d etl -s $store
   mysql -h $mysql -D etl -u $user -p$pass < etl/factory_summary_tables_$store.sql
 }
 
