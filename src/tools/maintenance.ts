5181645af1931f9308bd08c76df66e4f880ee894
Sudzy Group <support@sudzy.co>
2022-04-08 23:18:48 -0400

    Update maintenance.ts

--- src/tools/maintenance.ts
+++ src/tools/maintenance.ts
@@ -118,7 +118,7 @@ function sync(local, remote, cb) {
 		cb && cb();
 	}).on('paused', info => {
 		console.log('paused sync');
-		cb && cb();
+		// cb && cb();
 	}).on('change', info => {		
 		inProgress = true
 		console.log('Changed ', _.get(info, 'change.docs_read'), progress(info));
